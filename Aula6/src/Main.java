f1d2ef21165dc8628a2f3c90725d0587e46e46f6
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-20 19:48:57 -0300

    feat : compilando exemplos

---
+++ Aula6/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Instanciar uma classe
+        Cliente cliente1 = new Cliente("Lucas", 32);
+        Cliente cliente2 = new Cliente("Santiago", 42);
+
+        // Mostrar os dados
+        System.out.println("Nome: " + cliente1.getNome());
+        System.out.println("Idade: " + cliente1.getIdade());
+        System.out.println("Nome: " + cliente2.getNome());
+        System.out.println("Idade: " + cliente2.getIdade());
+
+        // Mostrar o toString()
+        System.out.println(cliente1.toString());
+        System.out.println(cliente2.toString());
+    }
+}
